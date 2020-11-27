15826a8cfaf4143626dd95a67a767b6a6bec80b6
Lucas SCHWAB <lucas.schwab@its4u.com>
2020-11-27 15:06:07 +0100

    Updated filters on selected project to change. New script to create the default template.

---
+++ to_already_created_projects.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if oc version > /dev/null 2> /dev/null; then
+    echo "oc found."
+else
+    echo "Please install oc."
+    exit 1
+fi
+
+
+if oc whoami > /dev/null 2> /dev/null; then
+    echo "Connected to cluster."
+else
+    echo "Not connected to cluster. Please run 'oc login' with administrator credentials."
+    exit 1
+fi
+
+
+echo "Applying NetworkPolicy on projects..."
+
+while read -r p; do
+    echo ">>Analysing project $p";
+    
+    ingress=true;
+    monitoring=true;
+    namespace=true;
+
+    while read -r np; do
+        case $np in
+            "allow-from-openshift-ingress") ingress=false;;
+            "allow-from-openshift-monitoring") monitoring=false;;
+            "allow-same-namespace") namespace=false;;
+            *) echo "unknown policy";;
+        esac
+    done < <(oc get networkpolicy -o name -n $p| sed 's:^networkpolicy.networking.k8s.io/::');
+
+    if [ "$ingress" = true ];then
+        oc create -n $p -f ./network-policies/allow-from-openshift-ingress.json;
+    fi
+    if [ "$monitoring" = true ];then
+        oc create -n $p -f ./network-policies/allow-from-openshift-monitoring.json;
+    fi
+    if [ "$namespace" = true ];then
+        oc create -n $p -f ./network-policies/allow-same-namespace.json;
+    fi
+done < <(oc projects -q | grep -vE '^(default$|openshift|kube)')
+
+
---
+++ to_new_projects_by_default.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if jq --version > /dev/null 2> /dev/null;then
+    echo "jq found.";
+else
+    echo "Please install jq";
+    exit 1
+fi
+
+
+if oc version > /dev/null 2> /dev/null;then
+    echo "oc found.";
+else
+    echo "Please install oc";
+    exit 1
+fi
+
+
+if oc whoami > /dev/null 2> /dev/null;then
+    echo "Already connected to cluster."
+else
+    echo "Please login to cluster using oc login"
+    exit 1
+fi
+
+
+echo "Retrieving default project template..."
+oc adm create-bootstrap-project-template -o json > default_template.json
+
+echo "Adding Network Policy to template..."
+jq '.objects += [$var1,$var2,$var3]' default_template.json \
+    --slurpfile var1 ./network-policies/allow-same-namespace.json \
+    --slurpfile var2 ./network-policies/allow-from-openshift-ingress.json \
+    --slurpfile var3 ./network-policies/allow-from-openshift-monitoring.json \
+    > new_default_template.json;
+
+
+exit 0
+
+echo "Uploading template to cluster..."
+oc create -f new_default_template.json -n openshift-config
+
+echo "Update cluster to use this template by default..."
+oc patch project.config.openshift.io/cluster --type=json \
+    -p='[{"op":"replace", "path":"/spec/projectRequestTemplate/name", "value":"project-request"}]'
+
+echo "Cleaning files..."
+rm ./default_template.json
+rm ./new_default_template.json
+
+echo ""
+echo "Done! create new project to verify changes have been made"
+echo "> oc new-project test-network-policy"
+echo "> oc get networkpolicy"
+echo "Should give 3 policies, 'allow-from-openshift-ingress', 'allow-from-openshift-monitoring', 'allow-same-namespace'
